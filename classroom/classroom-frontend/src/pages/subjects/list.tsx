4b5937ab3d5dab685daa7b51ec0c88803975a573
Dell <robert.maksymow@tutanota.com>
2026-06-30 17:10:45 +0100

    Displaying data. 2h18min

--- classroom/classroom-frontend/src/pages/subjects/list.tsx
+++ classroom/classroom-frontend/src/pages/subjects/list.tsx
@@ -1,7 +1,14 @@
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { ListView } from "@/components/refine-ui/views/list-view";
 import React from "react";
 
 const SubjectsList = () => {
-  return <div>Subjects List</div>;
+  return (
+    <ListView>
+      <Breadcrumb />
+      <h1 className="page-title">Subjects List</h1>
+    </ListView>
+  );
 };
 
 export default SubjectsList;
